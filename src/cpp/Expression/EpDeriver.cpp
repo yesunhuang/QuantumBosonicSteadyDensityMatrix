c5e1660c6903e98995823fa23ac90ccd72168f5a
YesunHuang <tgbus944812203@hotmail.com>
2021-02-16 00:08:28 +0800

    处理哈密顿量的解决

--- src/cpp/Expression/EpDeriver.cpp
+++ src/cpp/Expression/EpDeriver.cpp
@@ -35,10 +35,120 @@ struct RawTerm{
 };
 
 class EpDeriver{
-public:
-    EpDeriver(){
+private:
+    std::vector<RawTerm> H;
+    std::vector<RawTerm> co_ps;
+    std::vector<Term> masterEps;
+    std::vector<std::vector<int>> neignborIndexs;
+    std::vector<std::vector<Term>> neighborEps;
+    std::vector<int> rawIndex;
+
+    void buildHEps(){
+        //处理哈密顿量
+        for (std::vector<RawTerm>::iterator
+                rawTerm=(*this).H.begin();
+                rawTerm!=(*this).H.end();rawTerm++) {
+            // 左右一起处理了
+            std::vector<int> neighborIndexL=rawIndex;
+            std::vector<int> neighborIndexR=rawIndex;
+            std::vector<Factor> neignborTermL=std::vector<Factor>();
+            std::vector<Factor> neignborTermR=std::vector<Factor>();
+            for (std::vector<int>::iterator
+                    ops=(*rawTerm).termOp.begin();
+                    ops!=(*rawTerm).termOp.end();ops++){
+                int mode=(*ops-1)/2+1;
+                bool creat=((*ops)%2!=0);
+                if (neignborTermL.size()==0||mode!=(*neignborTermL.end()).mode){
+                    Factor factor={mode,0,0};
+                    neignborTermL.push_back(factor);
+                    neignborTermR.push_back(factor);
+                }
+                // 预处理算符作用
+                if (creat) {
+                    neighborIndexL[2*(mode-1)]--;
+                    neighborIndexR[2*mode-1]++;
+                    (*neignborTermR.end()).ket++;
+                }
+                else
+                {
+                    neighborIndexL[2*(mode-1)]++;
+                    neighborIndexR[2*mode-1]--;
+                    (*neignborTermL.end()).bra++;
+                }
+            }
+            Term termL={(*rawTerm).coef,ayaji::Complex(0,1),neignborTermL};
+            Term termR={(*rawTerm).coef,ayaji::Complex(0,-1),neignborTermR};
+            if (isSame(rawIndex,neighborIndexL)) {
+                masterEps.push_back(termL);
+                masterEps.push_back(termR);
+                continue;
+            }
+            int pos=findInside(neignborIndexs,neighborIndexL);
+            if (pos==-1)
+            {
+                std::vector<Term> newExpression=std::vector<Term>();
+                newExpression.push_back(termL);
+                neignborIndexs.push_back(neighborIndexL);
+                neighborEps.push_back(newExpression);
+            }
+            else
+                neighborEps[pos].push_back(termL);
+
+            pos=findInside(neignborIndexs,neighborIndexR);
+            if (pos==-1)
+            {
+                std::vector<Term> newExpression=std::vector<Term>();
+                newExpression.push_back(termR);
+                neignborIndexs.push_back(neighborIndexR);
+                neighborEps.push_back(newExpression);
+            }
+            else
+                neighborEps[pos].push_back(termR);
+        }
+    }
+
+    void buildCollapseEps(){
         //TODO
-    };
+    }
+
+    bool isSame(std::vector<int> a,std::vector<int> b) {
+        if (a.size()!=b.size()) return false;
+        for (int i=0;i<a.size();i++) 
+            if (a[i]!=b[i])
+                return false;
+        return true;
+    }
+
+    int findInside(std::vector<std::vector<int>> array,std::vector<int> target) {
+        for (int i=0;i<array.size();i++){
+            if(isSame(array[i],target))
+                return i;
+        }
+        return -1;
+    }
+public:
+    /**
+     * @name: EpDeriver(std::vector<RawTerm> hamiltonian,
+              std::vector<RawTerm> collapse,
+              std::vector<int> index)
+     * @fuction: 预处理
+     * @param {*hamiltonian} 哈密顿量
+     * @param {*collapse} 坍塌算符
+     * @param {*index} 全是零的索引
+     * @return {*EpDriver}
+     */
+    EpDeriver(std::vector<RawTerm> hamiltonian,
+              std::vector<RawTerm> collapse,
+              std::vector<int> index) {
+        (*this).H=hamiltonian;
+        (*this).co_ps=collapse;
+        (*this).rawIndex=index;
+        (*this).masterEps=std::vector<Term>();
+        (*this).neignborIndexs=std::vector<std::vector<int>>();
+        (*this).neighborEps=std::vector<std::vector<Term>>();
+        buildHEps();
+        buildCollapseEps();
+    }
     /**
      * @name: calEp(Expression expression,std::vector<int> index)
      * @fuction: 计算表达式值
