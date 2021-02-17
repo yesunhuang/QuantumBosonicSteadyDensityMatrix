c9616d512bc4ed5c53473ff13f7ab967ea4f5756
YesunHuang <tgbus944812203@hotmail.com>
2021-02-17 22:29:20 +0800

    修复指针问题

--- src/cpp/Expression/EpDeriver.cpp
+++ src/cpp/Expression/EpDeriver.cpp
@@ -19,7 +19,7 @@ struct Factor{
  * @fuction: 预处理后的项，一项由多个Factors组成
  */
 struct Term{
-    ayaji::Complex coef;
+    ayaji::Complex *coef;
     // 因为前一个只有引用，所以单独开了一个空间存乘在系数上的因子(比如-1)
     ayaji::Complex coefFactor;
     std::vector<Factor> factors;
@@ -73,8 +73,8 @@ private:
                     (*neignborTermL.end()).bra++;
                 }
             }
-            Term termL={(*rawTerm).coef,ayaji::Complex(0,1),neignborTermL};
-            Term termR={(*rawTerm).coef,ayaji::Complex(0,-1),neignborTermR};
+            Term termL={&(*rawTerm).coef,ayaji::Complex(0,1),neignborTermL};
+            Term termR={&(*rawTerm).coef,ayaji::Complex(0,-1),neignborTermR};
             if (isSame(rawIndex,neighborIndexL)) {
                 termL.coefFactor*=ayaji::Complex(-1,0);
                 termR.coefFactor*=ayaji::Complex(-1,0);
@@ -122,25 +122,24 @@ private:
             if (creat) {
                 neighborIndex[2*(mode-1)]-=1;
                 neighborIndex[2*mode-1]-=1;
-                termM={(*rawTerm).coef,ayaji::Complex(1,0),neighborTerm};
+                termM={&(*rawTerm).coef,ayaji::Complex(1,0),neighborTerm};
                 std::vector<Factor> TermLR=std::vector<Factor>();
-                Term termLR={(*rawTerm).coef,ayaji::Complex(1,0),TermLR};
+                Term termLR={&(*rawTerm).coef,ayaji::Complex(1,0),TermLR};
                 masterEps.push_back(termLR);
-            }
-            else {
+            } else {
                 neighborIndex[2*(mode-1)]+=1;
                 neighborIndex[2*mode-1]+=1;
                 Factor factorM={mode,1,1};
                 neighborTerm.push_back(factorM);
-                termM={(*rawTerm).coef,ayaji::Complex(1,0),neighborTerm};
+                termM={&(*rawTerm).coef,ayaji::Complex(1,0),neighborTerm};
                 Factor factorL={mode,1,0};
                 Factor factorR={mode,0,1};
                 std::vector<Factor> TermL=std::vector<Factor>();
                 std::vector<Factor> TermR=std::vector<Factor>();
                 TermL.push_back(factorL);
                 TermR.push_back(factorR);
-                Term termL={(*rawTerm).coef,ayaji::Complex(0.5,0),TermL};
-                Term termR={(*rawTerm).coef,ayaji::Complex(0.5,0),TermR};
+                Term termL={&(*rawTerm).coef,ayaji::Complex(0.5,0),TermL};
+                Term termR={&(*rawTerm).coef,ayaji::Complex(0.5,0),TermR};
                 masterEps.push_back(termL);
                 masterEps.push_back(termR);
             }
@@ -220,7 +219,7 @@ public:
                 term=expression.begin();
                 term!=expression.end();term++) {
             ayaji::Complex termVal=ayaji::Complex();
-            termVal+=((*term).coef*(*term).coefFactor);
+            termVal+=((*(*term).coef)*(*term).coefFactor);
             double realTermVal=1;
             // 遍历当前项中的不同因子
             for (std::vector<Factor>::iterator 
@@ -240,4 +239,28 @@ public:
         return result;
     }
 
+    /**
+     * @name: updateCoef
+     * @fuction: 更新系数,不用再预处理一次
+     * @param {*hamiltonian} 哈密顿量
+     * @return {*collapse} 坍塌算符
+     */
+    void updateCoef(std::vector<RawTerm> hamiltonian,
+                    std::vector<RawTerm> collapse){
+        if (hamiltonian.size()!=(*this).H.size()) {
+            throw "update Hamiltonian error!";
+        }
+        for (int i=0;i<hamiltonian.size();i++) {
+            (*this).H[i].coef.setReal(hamiltonian[i].coef.getReal());
+            (*this).H[i].coef.setImage(hamiltonian[i].coef.getImage());
+        }
+        if (collapse.size()!=(*this).co_ps.size()) {
+            throw "update Collapse error!";
+        }
+        for (int i = 0; i < collapse.size(); i++)
+        {
+            (*this).co_ps[i].coef.setReal(collapse[i].coef.getReal());
+            (*this).co_ps[i].coef.setImage(collapse[i].coef.getImage());
+        }
+    }
 };
