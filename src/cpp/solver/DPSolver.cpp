ab7b9d8e1b15b7b6b1c9f5d437c5d7d786fd50e0
guch8017 <guch8017@gmail.com>
2021-03-09 23:15:29 +0800

    Fix bugs.

--- src/cpp/solver/DPSolver.cpp
+++ src/cpp/solver/DPSolver.cpp
@@ -69,21 +69,26 @@ void DPSolver::doRun(int depth, const std::vector<int>& index) {
     int loopSize = matrixSizeArray[depth];
     // 主循环体
     // 所有数据都是从mapSrc读取写入到mapDst，故不会出现读写访问冲突，不加锁
-//#pragma omp parallel for
     for (int i = 0; i < loopSize; ++i) {
         for (int j = 0; j < loopSize; ++j) {
-            auto nInd = std::vector<int>(index);
-            nInd.push_back(i);
-            nInd.push_back(j);
-            doRun(depth + 1, nInd);
+#pragma omp task
+            {
+                auto nInd = std::vector<int>(index);
+                nInd.push_back(i);
+                nInd.push_back(j);
+                doRun(depth + 1, nInd);
+            }
         }
     }
+#pragma omp taskwait
 }
 
 bool DPSolver::run() {
     // 最外层循环为大迭代，不考虑并行关系
     for (int i = 0; i < maxRecurveTimes; ++i) {
         fit = true;
+#pragma omp parallel
+#pragma omp single
         doRun(0, std::vector<int>());
         std::swap(mapSrc, mapDst);
         if (fit) {
